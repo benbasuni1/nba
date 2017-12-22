def19344667bffab9f72f2c6bcdeb2695737620d
Ben Basuni <benbasuni1@gmail.com>
2017-12-21 21:11:54 -0800

    got react-bootstrap to render button

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Button } from 'react-bootstrap';
+import AwesomeComponent from './components/AwesomeComponent.jsx';
+import NavBar from './components/NavBarComponent.jsx';
+import Content from './components/ContentComponent.jsx';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React project</p>;
+    return (
+      <div>
+        <p>Hello React Project</p>
+        <AwesomeComponent />
+        <NavBar />
+        <Button>Default</Button>
+      </div>
+    )
   }
 }
 
 render(<App/>, document.getElementById('app'));
+
